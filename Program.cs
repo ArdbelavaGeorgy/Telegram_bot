6125811dfe55c2674aef95dff6029b2d3e0ba9a2
ArdbelavaGeorgy <147245423+ArdbelavaGeorgy@users.noreply.github.com>
2023-11-27 00:33:42 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
 
 // Создание нового клиента бота
-var botClient = new TelegramBotClient("6819742424:AAEKR5x9oAXywYEokhQlmGh6XX9X907JjhM");
+var botClient = new TelegramBotClient("AAFwsbtHlWK_QqgR3SmRMD_Dxcqf8ViJYHg");
 
 using CancellationTokenSource cts = new();
 
